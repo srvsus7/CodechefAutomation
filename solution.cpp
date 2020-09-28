c74ea3cacad388bc45c31f9c699a9d5bf6825ac6
srvsus7 <64803470+srvsus7@users.noreply.github.com>
2020-09-28 12:02:53 +0530

    Add files via upload

---
+++ solution.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int num;
+    while((cin>>num)&&(num!=42)){
+        cout<<num<<endl;
+    }
+return 0;
+}
+
